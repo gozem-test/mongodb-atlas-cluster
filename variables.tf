328f5551351b976857ce7fddfe7e21fb81902db4
Stéphane Noutsa <stephanenoutsak@gmail.com>
2023-11-25 02:52:32 +0100

    Adds AWS credentials vars

--- variables.tf
+++ variables.tf
@@ -1,3 +1,20 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_SESSION_TOKEN" {
+  type    = string
+  default = null
+}
+
+variable "AWS_REGION" {
+  type = string
+}
+
 variable "MONGODB_ATLAS_PUBLIC_KEY" {
   type = string
 }
