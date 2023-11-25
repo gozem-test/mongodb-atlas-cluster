7fa2613f627b0a55fe3cf97f747cfbb8d3ec6dd7
Stéphane Noutsa <stephanenoutsak@gmail.com>
2023-11-25 03:17:31 +0100

    Fixes output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "standard" {
-  value = mongodbatlas_cluster.cluster.connection_strings[0].standard
+  # value = mongodbatlas_cluster.cluster.connection_strings[0].standard
+  value = mongodbatlas_advanced_cluster.cluster.connection_strings[0].standard
 }
